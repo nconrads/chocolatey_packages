411838d1074080ecac9768c0b75fb26d23e266a6
Nick Conrads <nconrads@alum.bucknell.edu>
2013-04-18 17:50:24 -0400

    Added chocolatey package specs for blender, jitsi, mumble, pyhoca, qbittorrent, and zotero-standalone.

---
+++ mumble/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'mumble' 
+$installerType = 'MSI' 
+$url = 'http://sourceforge.net/projects/mumble/files/Mumble/1.2.3a/mumble-1.2.3a.msi/download' # download url
+$silentArgs = '/quiet' 
+$validExitCodes = @(0) 
+
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
